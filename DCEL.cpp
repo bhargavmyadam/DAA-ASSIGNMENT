cf935242a178acf6b9ed2956f27d95d58afd9830
bhargavmyadam <74249805+bhargavmyadam@users.noreply.github.com>
2023-02-24 13:32:56 +0530

    Add files via upload

---
+++ DCEL.cpp
@@ -0,0 +1,31 @@
+/*
+x1,y1 x2,y2 x3,y3  ............... xn,yn
+*/
+
+class DCEL{
+    vector<Vertex> vertices;
+    vector<Face> faces;
+
+
+    //helper functions
+    addVertex(int x1,int y1){
+
+    }
+
+
+    addEdge(Vertex v1,Vertex v2){
+
+    }
+}
+
+struct Vertex{
+    
+}
+
+struct Edge{
+   // 
+}
+
+struct Face{
+
+}
